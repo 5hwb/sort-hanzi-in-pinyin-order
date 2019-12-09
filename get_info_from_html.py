75f44c03d26102692e03cf207bdca4aeab1f9fd3
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-12-10 00:53:52 +1100

    Extract main pinyin pronunciation

--- get_info_from_html.py
+++ get_info_from_html.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 
 # Read HTML file and show contents
 with open("The most common Chinese characters (Unicode).html", "r+") as file:
@@ -7,15 +8,40 @@ with open("The most common Chinese characters (Unicode).html", "r+") as file:
 soup = BeautifulSoup(html_doc, 'html.parser')
 #print(soup.prettify())
 
+intended_content = []
+
 # Go thru each table row (limit to 10 for now)
+row_num = 0
 for tr_content in soup.body.blockquote.table.tbody.find_all('tr')[:10]:
-    print("===== CONTENTS: =====")
+    
+    # Skip 1st row - not useful info
+    if (row_num == 0):
+        row_num += 1
+        continue
+    
+    row_tuple = []
+    print("===== ROW {} CONTENTS: =====".format(row_num))
+    
     # Go thru each cell in the row
+    cell_num = 0
     for td_content in tr_content.find_all('td'):
         # Go thru each sub-tag in the cell, and create a string out of their contents
+        
         td_subcontent = td_content.contents
-        td_subcontent_string = ";;;;;"
+        td_subcontent_string = ""
         for subcontent in td_subcontent:
             td_subcontent_string += subcontent.string
-        td_subcontent_string += ",,,,,"
-        print(td_subcontent_string)
+        
+        # Process the 'Pronunciations' string - get only the pinyin reading
+        if (cell_num == 2):
+            td_subcontent_string = re.findall("\[(.+?)\]", td_subcontent_string)[0]
+        
+        row_tuple.append(td_subcontent_string)
+        cell_num += 1
+    
+    intended_content.append(row_tuple)
+    row_num += 1
+
+for aa in intended_content:
+    print("===== CONTENTS ({}): =====".format(len(aa)))
+    print(aa)
