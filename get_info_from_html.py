be789b49490830d2cf37948b81c83f09f506f900
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-12-10 00:29:59 +1100

    Extract data from 'The most common Chinese characters'

--- get_info_from_html.py
+++ get_info_from_html.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 
 # Read HTML file and show contents
-with open("someFairyTale.html", "r+") as file:
+with open("The most common Chinese characters (Unicode).html", "r+") as file:
     html_doc = file.read()           # reads a string from a file
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 #print(soup.prettify())
 
-for aa in soup.find_all('a'):
-    print(aa.contents)
-    
-
+# Go thru each table row (limit to 10 for now)
+for tr_content in soup.body.blockquote.table.tbody.find_all('tr')[:10]:
+    print("===== CONTENTS: =====")
+    # Go thru each cell in the row
+    for td_content in tr_content.find_all('td'):
+        # Go thru each sub-tag in the cell, and create a string out of their contents
+        td_subcontent = td_content.contents
+        td_subcontent_string = ";;;;;"
+        for subcontent in td_subcontent:
+            td_subcontent_string += subcontent.string
+        td_subcontent_string += ",,,,,"
+        print(td_subcontent_string)
