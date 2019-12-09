bb77a9c0b8c1ab1e2fe7f03e71612c708be06407
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-12-09 23:16:46 +1100

    Get list of links in the BeautifulSoup sample HTML page - success

--- get_info_from_html.py
+++ get_info_from_html.py
@@ -3,5 +3,11 @@ from bs4 import BeautifulSoup
 # Read HTML file and show contents
 with open("someFairyTale.html", "r+") as file:
     html_doc = file.read()           # reads a string from a file
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    print(soup.prettify())
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+#print(soup.prettify())
+
+for aa in soup.find_all('a'):
+    print(aa.contents)
+    
+
