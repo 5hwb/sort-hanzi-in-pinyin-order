e851b13ca6bbdc815b1c51ceb08c9f6a710d1d73
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2019-12-10 15:31:36 +1100

    Add pinyin sorting order

--- get_info_from_html.py
+++ get_info_from_html.py
@@ -1,6 +1,37 @@
 from bs4 import BeautifulSoup
 import re
 
+# Pinyin sort order
+
+pinyin_initials = \
+["","b","p","f","m","d","t","n","l",
+"g","k","h","j","q","x",
+"zh","ch","sh","r","z","c","s",
+"y","w"]
+
+pinyin_rimes = \
+["", "a", "ā", "á", "ǎ", "à", "o", "ō", "ó", "ǒ", "ò", 
+"e", "ē", "é", "ě", "è", "i", "ī", "í", "ǐ", "ì", 
+"u", "ū", "ú", "ǔ", "ù", "ü", "ǖ", "ǘ", "ǚ", "ǜ", 
+"ai", "āi", "ái", "ǎi", "ài", "ei", "ēi", "éi", "ěi", "èi",
+"ui", "uī", "uí", "uǐ", "uì", "ao", "āo", "áo", "ǎo", "ào",
+"ou", "ōu", "óu", "ǒu", "òu", "iu", "iū", "iú", "iǔ", "iù",
+"ie", "iē", "ié", "iě", "iè", "ue", "uē", "ué", "uě", "uè",
+"üe", "üē", "üé", "üě", "üè", "er", "ēr", "ér", "ěr", "èr",
+"an", "ān", "án", "ǎn", "àn", "ang", "āng", "áng", "ǎng", "àng",
+"en", "ēn", "én", "ěn", "èn", "eng", "ēng", "éng", "ěng", "èng",
+"in", "īn", "ín", "ǐn", "ìn", "ing", "īng", "íng", "ǐng", "ìng",
+"ong", "ōng", "óng", "ǒng", "òng", "un", "ūn", "ún", "ǔn", "ùn",
+"ia", "iā", "iá", "iǎ", "ià", "iao", "iāo", "iáo", "iǎo", "iào",
+"ian", "iān", "ián", "iǎn", "iàn", "iang", "iāng", "iáng", "iǎng", "iàng",
+"iong", "iōng", "ióng", "iǒng", "iòng",
+"ua", "uā", "uá", "uǎ", "uà", "uo", "uō", "uó", "uǒ", "uò",
+"uai", "uāi", "uái", "uǎi", "uài", "uan", "uān", "uán", "uǎn", "uàn",
+"uang", "uāng", "uáng", "uǎng", "uàng"]
+
+# TODO: parse a Pinyin syllable and convert it into a tuple of numbers.
+# e.g. 'you' = [3, 65] where y = 3 and ou = 65.
+
 # Read HTML file and show contents
 with open("The most common Chinese characters (Unicode).html", "r+") as file:
     html_doc = file.read()           # reads a string from a file
